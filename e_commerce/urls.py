de7018a39725a8cfc580df7d53f49914e7357f9e
Arleking <Abdukarim600@gmail.com>
2025-12-19 16:42:31 +0500

    update admin link

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('websec/admin/', admin.site.urls),
     path('websec/api/auth/', include('user.urls')),
     path('websec/', include('shop.urls')),
 ]
