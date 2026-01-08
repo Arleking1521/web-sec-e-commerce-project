722415d6477f9aa910d5581754bf7c2706ed72e8
Arleking <Abdukarim600@gmail.com>
2026-01-08 18:23:54 +0500

    update fk order-product

--- shop/models.py
+++ shop/models.py
@@ -118,7 +118,7 @@ class OrderItem(models.Model):
     )
     product = models.ForeignKey(
         Product,
-        on_delete=models.PROTECT,
+        on_delete=models.CASCADE,
         related_name="order_items"
     )
     quantity = models.PositiveIntegerField(default=1)
