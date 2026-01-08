351e630353a1dda3c8772b29b257954fd926ee9b
Arleking <Abdukarim600@gmail.com>
2026-01-08 17:21:13 +0500

    update  media link

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -137,7 +137,7 @@ LOCALE_PATHS = [
     os.path.join(BASE_DIR, 'locale')
 ]
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/websec/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
